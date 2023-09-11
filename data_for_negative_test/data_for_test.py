73c95f414142c5624bde25a7e302f488e167e6c6
Kirill Gaposhkin <130587425+Kirill5github@users.noreply.github.com>
2023-09-11 19:48:31 +0300

    Add files via upload

---
+++ data_for_negative_test/data_for_test.py
@@ -0,0 +1,58 @@
+class DataForTests:
+
+    name = 'Кирилл'
+    last_name = 'Гапошкин'
+
+    valid_email = 'test@mail.ru'  #  Верная почта, указать свою
+    valid_password = '1234'  # Верный пароль, указать свой
+
+    invalid_phone_number = '89000000000'  #  Неверный номер телефона
+    phone_number_less_11_numbers = '8900000000'  #  10 цифр
+    phone_number_more_11_numbers = '890000000000'  #  12 цифр
+
+    invalid_mail = 'invalid.test@mail.ru'  # Незарегестрированный Email
+    invalid_password = 'test'  #  Незарегестрированный пароль
+
+    invalid_login = 'testing123'  #  НЕверный логин
+
+    invalid_personal_account = '111111111111'  #  Неверный лицевой счет
+    personal_account_less_12_numbers = '11111111111'  # 11 цифр
+    personal_account_more_12_numbers = '1111111111111'  # 13 цифр
+
+    kirilica = 'лотщцоимфукидымваилуаоимфшу'
+    latinica = 'skjbwkencowekvbwrvlbrowir'
+    spec_simbols = '!@#$%^&*()_+'
+    simbols_255 = 'jjrovrovebovdrvajdrobeoruhbvoeiborerovbeorb39474bv9u59bv3ierbviurbwivbiurbv38bvbrbviwerbviuv394isjbribrvibrijbroveubobvobrouvbosrubeourbiuevbiebibrovwurbovuebibrowubrivubeoriueborubovrboeubrovueborubrovuboeubrovubeorubeorubvoeubroeubrowvrborovboerbvowubvr'
+    simbols_1001 = 'jjrovrovebovdrvajdrobeoruhbvoeiborerovbeorb39474bv9u59bv3ierbviurbwivbiurbv38bvbrbviwerbviuv394isjbribrvibrijbroveubobvobrouvbosrubeourbiuevbiebibrovwurbovuebibrowubrivubeoriueborubovrboeubrovueborubrovuboeubrovubeorubeorubvoeubroeubrowvrborovboerbvowubvrjjrovrovebovdrvajdrobeoruhbvoeiborerovbeorb39474bv9u59bv3ierbviurbwivbiurbv38bvbrbviwerbviuv394isjbribrvibrijbroveubobvobrouvbosrubeourbiuevbiebibrovwurbovuebibrowubrivubeoriueborubovrboeubrovueborubrovuboeubrovubeorubeorubvoeubroeubrowvrborovboerbvowubvrjjrovrovebovdrvajdrobeoruhbvoeiborerovbeorb39474bv9u59bv3ierbviurbwivbiurbv38bvbrbviwerbviuv394isjbribrvibrijbroveubobvobrouvbosrubeourbiuevbiebibrovwurbovuebibrowubrivubeoriueborubovrboeubrovueborubrovuboeubrovubeorubeorubvoeubroeubrowvrborovboerbvowubvrjjrovrovebovdrvajdrobeoruhbvoeiborerovbeorb39474bv9u59bv3ierbviurbwivbiurbv38bvbrbviwerbviuv394isjbribrvibrijbroveubobvobrouvbosrubeourbiuevbiebibrovwurbovuebibrowubrivubeoriueborubovrboeubrovueborubrovuboeubrovubeorubeorubvoeubroeubrow'
+
+    """Данные для поля имя, фамилия в форме регистрации"""
+
+    letter_kirillica_2 = 'аб'
+    letter_latinica_2 = 'df'
+    letter_kirillica_1 = 'б'
+    letter_latinica_1 = 'f'
+    letter_kirillica_29 = 'абвгдежзийклмнопрстуфхцчшщэюя'
+    letter_latinica_29 = 'abcdefghijklmnopqrstuvwxyzzzz'
+    letter_kirillica_30 = 'абвгдежзийклмнопрстуфхцчшщэюяя'
+    letter_latinica_30 = 'abcdefghijklmnopqrstuvwxyzzzzz'
+    letter_kirillica_31 = 'абвгдежзийклмнопрстуфхцчшщэюяяя'
+    letter_latinica_31 = 'abcdefghijklmnopqrstuvwxyzzzzzz'
+    simbols_255_kirillica = 'лыуикдмшфикмфшциывлоиыдмщугкжимшгуигкфрпушгтмкрфжущшпмнжкщыукржщпмгшыружкгыржукгпшржущадладишрзывиыащгаыужкщиыжукгржмщыимшфгившмциывдоиащгищмфищгщгыкмщыиущшгмщыивмышгкищымищкгимыоивщгумкщгпыиугмицдокищгыищугиыщмгщкгищыгуивмщышгупмщгкищмгыщмыщгпщыгиумщгщго'
+    simbols_1001_kirillica = 'лыуикдмшфикмфшциывлоиыдмщугкжимшгуигкфрпушгтмкрфжущшпмнжкщыукржщпмгшыружкгыржукгпшржущадладишрзывиыащгаыужкщиыжукгржмщыимшфгившмциывдоиащгищмфищгщгыкмщыиущшгмщыивмышгкищымищкгимыоивщгумкщгпыиугмицдокищгыищугиыщмгщкгищыгуивмщышгупмщгкищмгыщмыщгпщыгиумлыуикдмшфикмфшциывлоиыдмщугкжимшгуигкфрпушгтмкрфжущшпмнжкщыукржщпмгшыружкгыржукгпшржущадладишрзывиыащгаыужкщиыжукгржмщыимшфгившмциывдоиащгищмфищгщгыкмщыиущшгмщыивмышгкищымищкгимыоивщгумкщгпыиугмицдокищгыищугиыщмгщкгищыгуивмщышгупмщгкищмгыщмыщгпщыгиумлыуикдмшфикмфшциывлоиыдмщугкжимшгуигкфрпушгтмкрфжущшпмнжкщыукржщпмгшыружкгыржукгпшржущадладишрзывиыащгаыужкщиыжукгржмщыимшфгившмциывдоиащгищмфищгщгыкмщыиущшгмщыивмышгкищымищкгимыоивщгумкщгпыиугмицдокищгыищугиыщмгщкгищыгуивмщышгупмщгкищмгыщмыщгпщыгиумлыуикдмшфикмфшциывлоиыдмщугкжимшгуигкфрпушгтмкрфжущшпмнжкщыукржщпмгшыружкгыржукгпшржущадладишрзывиыащгаыужкщиыжукгржмщыимшфгившмциывдоиащгищмфищгщгыкмщыиущшгмщыивмышгкищымищкгимыоивщгумкщгпыиугмицдокищгыищугиыщмгщкгищыгуивмщышгупмщгкищмгыщмыщгпщыгиумd'
+
+    name_with_tire = '-Кирилл'
+    name_with_tire_between = 'Кир-илл'
+
+    lastname_with_tire = '-Гапошкин'
+    lastname_with_tire_between = 'Га-Пошкин'
+
+    numbers = '6545442763582748232753527539729236'
+
+    """Данные для полей пароль и подтверждение пароля"""
+
+    password_kirillica = 'Пароль123@'
+    password_21 = 'Testingpass111ddddddd'
+    password_specsimbols = '!@#$%^&*()_'
+    password_numbers = '1234567890'
+    password_less_8_numbers = 'Te$t111'
+    password_caps = 'TESTINGPASS111'
